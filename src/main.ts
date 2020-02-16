90d29a1064e6f06bf7eb5cb45d9035cc04556a99
tonymontana97 <Pavlo2013>
2020-02-16 16:26:58 +0200

    added linter settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import 'dotenv/config'
+import 'dotenv/config';
+import * as RateLimit from 'express-rate-limit';
+import * as helmet from 'helmet';
+import * as morgan from 'morgan';
+import * as compression from 'compression';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as Express from 'express';
-import { ExpressAdapter } from '@nestjs/platform-express';
+import {
+    ExpressAdapter,
+    NestExpressApplication,
+} from '@nestjs/platform-express';
+import { setupSwagger } from './swagger';
 
 /*
  * Google Cloud additional endpoints
@@ -12,11 +20,28 @@ server.get('/', (req, res) => res.send('ok'));
 server.get('/_ah/health', (req, res) => res.send('ok'));
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    const app = await NestFactory.create<NestExpressApplication>(
+        AppModule,
+        new ExpressAdapter(server),
+        { cors: true },
+    );
 
-  app.setGlobalPrefix('api')
+    app.use(helmet()); // protect http headers
+    app.use(
+        new RateLimit({
+            windowMs: 15 * 60 * 1000, // 15 minutes
+            max: 100, // limit each IP to 100 requests per windowMs
+        }),
+    );
+    app.use(compression()); // for less size of response
+    app.use(morgan('combined')); // http requests logger
 
-  console.log('Runned on', process.env.PORT);
-  await app.listen(process.env.PORT);
+    // setup swagger
+    setupSwagger(app);
+
+    app.setGlobalPrefix('api');
+    await app.listen(process.env.PORT);
+
+    console.info(`server running on port ${process.env.PORT}`);
 }
 bootstrap();
